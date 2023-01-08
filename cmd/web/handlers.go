0f8465de07c8f7fd9ee64fde340f7c10824a599d
Muluneh Derese <muluneh.derese@protonmail.com>
2023-01-08 10:02:41 +0300

    project structure and organization

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,21 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
-}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
