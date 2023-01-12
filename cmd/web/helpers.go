c0a5314dd6716710e4bd2d9b964953709d137caa
Muluneh Derese <muluneh.derese@protonmail.com>
2023-01-12 20:15:03 +0300

    add runtime error catching

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -26,11 +27,17 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 	if !ok {
 		err := fmt.Errorf("The template %s does not exist", page)
 		app.serverError(w, err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buf, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+		return
 	}
 
 	w.WriteHeader(status)
-	err := ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serverError(w, err)
-	}
+	buf.WriteTo(w)
 }
