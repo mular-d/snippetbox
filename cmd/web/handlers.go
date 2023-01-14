3f0c5121614c65507680f0238170b823de3c633e
Muluneh Derese <muluneh.derese@protonmail.com>
2023-01-14 20:20:29 +0300

    using session data to display flash message

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -32,6 +32,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
+
 	snippet, err := app.snippets.Get(id)
 	if err != nil {
 		if errors.Is(err, models.ErrNoRecord) {
@@ -90,6 +91,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
