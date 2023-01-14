3fc060e6900296ee7b057e58e3dadd387f0ecc56
Muluneh Derese <muluneh.derese@protonmail.com>
2023-01-14 20:48:10 +0300

    using https server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -51,6 +51,8 @@ func main() {
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
 
+	sessionManager.Cookie.Secure = true
+
 	app := &application{
 		errorLog:       errorLog,
 		infoLog:        infoLog,
@@ -67,7 +69,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
