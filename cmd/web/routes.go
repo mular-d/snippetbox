8ff0752ace94a29599d10d108f983d7e0fd49995
Muluneh Derese <muluneh.derese@protonmail.com>
2023-01-15 16:22:40 +0300

    embed all static files during build time

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"github.com/targerian1999/snippetbox/ui"
+
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
@@ -14,8 +16,8 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
