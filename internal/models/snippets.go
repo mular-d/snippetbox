3b780d2ba28b95c781000e0295dbae6b3bca6346
Muluneh Derese <muluneh.derese@protonmail.com>
2023-01-15 21:20:51 +0300

    mocking dependencies to test snippetView handler

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -6,9 +6,12 @@ import (
 	"time"
 )
 
-// Define a Snippet type to hold the data for an individual snippet. Notice how
-// the fields of the struct correspond to the fields in our MySQL snippets
-// table?
+type SnippetModelInterface interface {
+	Insert(title string, content string, expires int) (int, error)
+	Get(id int) (*Snippet, error)
+	Latest() ([]*Snippet, error)
+}
+
 type Snippet struct {
 	ID      int
 	Title   string
@@ -17,12 +20,10 @@ type Snippet struct {
 	Expires time.Time
 }
 
-// Define a SnippetModel type which wraps a sql.DB connection pool.
 type SnippetModel struct {
 	DB *sql.DB
 }
 
-// This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
 	stmt := `INSERT INTO snippets (title, content, created, expires)
 	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
